d8c1b1f992d310d1b1c1c3acb007fdf3401dffc6
suihandong-vicky <vickydong025@gmail.com>
2022-04-13 15:53:26 -0500

    basic templates

--- hocus-focus/src/router/index.js
+++ hocus-focus/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import TaskView from '../views/TaskView.vue'
+import RankingView from '../views/RankingView.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,17 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/tasks',
+    name: 'tasks',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: TaskView
+  },
+  {
+    path: '/ranking',
+    name: 'ranking',
+    component: RankingView
   }
 ]
 
