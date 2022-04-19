3efc64e078a0fbb185b826c2832a7f557dbcea8f
suihandong <chuyuanqi@gmail.com>
2022-04-19 18:20:28 -0500

    fix type error

--- hocus-focus/src/components/TomatoTimer.vue
+++ hocus-focus/src/components/TomatoTimer.vue
@@ -115,12 +115,13 @@
 					<button>Timer</button>
 					<!-- <button disabled> Theme </button> -->
 					<br/>
-					<h4> promodora duration : <editable-span :text="this.promdo" @edited="updatePromo"/></h4> 
+					<h4> promodora duration : <editable-span :text="this.promdo.toString()" @edited="updatePromo"/></h4> 
 					<br/>
-					<h4> short break duration : <editable-span :text="this.short" @edited="updateShort"/></h4><br/>
-					<h4> long break duration : <editable-span :text="this.long" @edited="updateLong"/></h4><br/>
-					<h4> long break delay : <editable-span :text="this.delay" @edited="updateDelay"/></h4><br/>
-					<h4> atuo start breaks : <span @click="auto_start_break = !auto_start_break"> {{this.auto_start_break}}</span></h4><br/>
+					<h4> short break duration : <editable-span :text="this.short.toString()" @edited="updateShort"/></h4><br/>
+					<h4> long break duration : <editable-span :text="this.long.toString()" @edited="updateLong"/></h4><br/>
+					<h4> long break delay : <editable-span :text="this.delay.toString()" @edited="updateDelay"/></h4><br/>
+					<h4> atuo start breaks : <span @click="auto_start_break = !auto_start_break">
+						{{this.auto_start_break}}</span></h4><br/>
 					<button @click="seen = !seen"> Close X </button>
 			</div>
 		</div>
@@ -224,16 +225,16 @@ export default {
 			}, 1000);
 		},
 		updatePromo: function(newPromo) {
-			this.promdo = newPromo
+			this.promdo = parseInt(newPromo)
 		},
 		updateShort: function(newShort) {
-			this.short = newShort
+			this.short = parseInt(newShort)
 		},
 		updateLong: function(newLong) {
-			this.long = newLong
+			this.long = parseInt(newLong)
 		},
 		updateDelay: function(newDelay) {
-			this.delay = newDelay
+			this.delay = parseInt(newDelay)
 		}
 	},
 };
