cd9f4547b6e19826e641c60d59f56f58456a9e33
kinzaahmed-web <kinza1013@yahoo.com>
2022-04-24 18:33:30 -0500

    updated the settings

--- hocus-focus/src/components/TimerSettings.vue
+++ hocus-focus/src/components/TimerSettings.vue
@@ -1,96 +1,103 @@
 <template>
 	<div v-if="show">
-		<button style="padding-top: 5%; padding-bottom: 3%; font-size: medium;">Timer</button>
+		<button>Timer</button>
 		<!-- <button disabled> Theme </button> -->
 		<br />
-		<div style="padding-bottom: 3%; font-size: medium; text-align: start; text-justify: auto;">
-			pomodoro duration
-			<span style="margin-left:7%">:</span>
-		
-		<span class="clickable" @click="settings.pomodoro -= 1"> - </span>
-		<editable-span
-			:text="this.settings.pomodoro.toString()"
-			@edited="settings.pomodoro = $event"
-		/>
-		<span class="clickable" @click="settings.pomodoro += 1"> + </span>
-		</div>
-		
-	
-		<div style="padding-bottom: 3%; font-size: medium; text-justify: auto; text-align: start;">
-			short break duration:
-		
-		<span class="clickable" @click="settings.short -= 1"> - </span>
-		<editable-span
-			:text="this.settings.short.toString()"
-			@edited="settings.short = $event"
-		/>
-		<span class="clickable" @click="settings.short += 1"> + </span>
+		<div
+			class="grid grid-cols-6 gap-4 p-5"
+			v-for="setting in settingsLabel"
+			:key="setting.label"
+		>
+			<div class="col-span-3 card">{{ setting.label }}</div>
+			<div class="col-span-1 text-2xl p-3 my-2">:</div>
+			<div class="col-span-1 card">
+				{{ settings[setting.field] }}
+			</div>
+			<div>
+				<BaseIcon
+					@clicked="incrementSettings(setting.field)"
+					:properties="{
+						height: 'h-8',
+						width: 'w-8',
+						strokeWidth: '1.5',
+						classes: ['stroke-pastel-green-500'],
+					}"
+					:dArray="['M5 15l7-7 7 7']"
+				/>
+				<BaseIcon
+					@clicked="decrementSettings(setting.field)"
+					:properties="{
+						height: 'h-8',
+						width: 'w-8',
+						strokeWidth: '1.5',
+						classes: ['stroke-pastel-green-500'],
+					}"
+					:dArray="['M19 9l-7 7-7-7']"
+				/>
+			</div>
 		</div>
 
-		<div style="padding-bottom: 3%; font-size: medium; text-justify: auto; text-align: start;">
-			long break duration :
-		
-		<span class="clickable" @click="settings.long -= 1"> - </span>
-		<editable-span
-			:text="this.settings.long.toString()"
-			@edited="settings.long = $event"
-		/>
-		<span class="clickable" @click="settings.long += 1"> + </span>
+		<div class="grid grid-cols-4 gap-4 p-5">
+			<div class="col-span-3 card">auto start breaks</div>
+			<div class="col-span-1 p-3 my-2">
+				<toggle-button
+					:value="settings.autobreak"
+					color="#82C7EB"
+					:labels="true"
+					@change="settings.autobreak = !settings.autobreak"
+				/>
+			</div>
 		</div>
-		
-
-		<div style="padding-bottom: 3%; font-size: medium; text-justify: auto; text-align: start;">
-			long break delay 
-			<span style="margin-left:10.5%">:</span>
-		<span class="clickable" @click="settings.delay -= 1"> - </span>
-		<editable-span
-			:text="this.settings.delay.toString()"
-			@edited="this.settings.delay = $event"
-		/>
-		<span class="clickable" @click="settings.delay += 1"> + </span></div>
-		
-
-		<div style="padding-bottom: 3%; font-size: medium; text-justify: auto; text-align: start;">
-			goal cycles 
-			<span style="margin-left:28.5%">:</span>
-		<span class="clickable" @click="settings.goalCycles -= 1"> - </span>
-		<editable-span
-			:text="this.settings.goalCycles.toString()"
-			@edited="this.settings.goalCycles = $event"
-		/>
-		<span class="clickable" @click="settings.goalCycles += 1"> + </span></div>
-		
-
-		<div style="padding-bottom: 3%; font-size: medium; text-justify: auto; text-align: start;">
-			auto start breaks 
-			<span style="margin-left:7%">: 
-				<!-- this part is for debug  -->
-				<!-- {{this.settings.autobreak}} -->
-			</span>	
-			
-			<toggle-button :value="true" color="#82C7EB"  :labels="true" @change="settings.autobreak = !settings.autobreak"/> 
-					
-			<!-- <span style="padding-left: 2%" class="clickable" @click="settings.autobreak = !settings.autobreak">
-				{{ settings.autobreak }}
-			</span> -->
+		<br />
+		<div title="Save Changes" class="close">
+			<BaseIcon
+				@clicked="update"
+				:properties="{
+					height: 'h-8',
+					width: 'w-8',
+					strokeWidth: '1.5',
+					classes: ['stroke-pastel-green-500'],
+				}"
+				:dArray="[
+					'M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z',
+				]"
+			/>
 		</div>
-		
-		
-		<button style="padding-top: 5%; font-size: medium;" @click="update">Close X</button>
 	</div>
 </template>
 
 <script>
 import { auth, db } from "@/main";
 import { pomodoro } from "@/constants";
-import EditableSpan from "@/components/EditableSpan.vue";
-
+import BaseIcon from "@/components/BaseIcon.vue";
 
 export default {
 	name: "TimerSettings",
 	data() {
 		return {
 			settings: pomodoro.DEFAULT_SETTINGS,
+			settingsLabel: [
+				{
+					label: "pomodoro duration",
+					field: "pomodoro",
+				},
+				{
+					label: "short break duration",
+					field: "short",
+				},
+				{
+					label: "long break duration",
+					field: "long",
+				},
+				{
+					label: "long break delay",
+					field: "delay",
+				},
+				{
+					label: "goal # of cycles",
+					field: "goalCycles",
+				},
+			],
 		};
 	},
 	props: {
@@ -98,7 +105,7 @@ export default {
 	},
 	emits: ["close"],
 	components: {
-		EditableSpan,
+		BaseIcon,
 	},
 
 	mounted() {
@@ -106,6 +113,12 @@ export default {
 	},
 
 	methods: {
+		incrementSettings(field) {
+			this.settings[field]++;
+		},
+		decrementSettings(field) {
+			this.settings[field]--;
+		},
 		fetchSettings() {
 			if (auth.currentUser) {
 				this.firestoreRef = db
@@ -132,10 +145,38 @@ export default {
 		},
 
 		update() {
-			// CTN_TODO: save settings *on close*
-			// CTN_TODO: emit to parent so timer manager can fetch new settings
-			this.$emit("close");
+			if (auth.currentUser) {
+				this.firestoreRef = db
+					.collection("users")
+					.doc(auth.currentUser.uid)
+					.collection("timer_settings");
+				const settingsDoc = this.firestoreRef.doc("0");
+				return settingsDoc
+					.update({
+						autobreak: this.settings.autobreak,
+						delay: this.settings.delay,
+						long: this.settings.long,
+						pomodoro: this.settings.pomodoro,
+						short: this.settings.short,
+						goalCycles: this.settings.goalCycles,
+					})
+					.then(() => {
+						console.log("Document successfully updated!");
+					})
+					.catch((error) => {
+						// The document probably doesn't exist.
+						console.error("Error updating document: ", error);
+					});
+			} else {
+				alert("Log in to Save Preferences!");
+			}
 		},
 	},
 };
 </script>
+<style scoped>
+.close {
+	display: flex;
+	justify-content: center;
+}
+</style>
