b60d74e3e0943482433c32f36f53d7cb0afacd48
Christina Nguyen <christina@terrier.tech>
2022-04-27 09:50:14 -0500

    only show prompt if permissions not already granted

--- hocus-focus/src/components/NotificationPermissions.vue
+++ hocus-focus/src/components/NotificationPermissions.vue
@@ -10,6 +10,13 @@ export default {
             granted: false,
         }
     },
+    mounted() {
+        if ('Notification' in window) {
+            this.granted = Notification.permission === 'granted';
+        } else {
+            console.log('show modal asking user for permission');
+        }
+    },
     methods: {
         requestPermission() {
             if ('Notification' in window) {
