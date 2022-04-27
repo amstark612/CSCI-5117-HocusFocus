b8a0af4901edb0818ade6462c79a871590c64e11
suihandong <chuyuanqi@gmail.com>
2022-04-27 11:07:01 -0500

    update close button

--- hocus-focus/src/components/TimerSettingModal.vue
+++ hocus-focus/src/components/TimerSettingModal.vue
@@ -77,7 +77,7 @@
                             </div>
                         </div>
                         <br />
-                        <div title="Save Changes" class="close">
+                        <div title="Save Changes" class="close" data-bs-dismiss="modal">
                             <BaseIcon
                                 @clicked="save"
                                 :properties="{
@@ -94,14 +94,14 @@
                     </div>
 
                     <!-- Modal Footer: Close popup -->
-                    <div
+                    <!-- <div
                         class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
                         <button type="button"
                         class="inline-block px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
                         data-bs-dismiss="modal">
                         Close
                         </button>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
