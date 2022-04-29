7710d40618e259edce3fab94fe2a1f39ec0cb6e3
Christina Nguyen <christina@terrier.tech>
2022-04-29 09:20:38 -0500

    error handling/handle browser not supporting notifications

--- hocus-focus/src/components/NotificationPermissions.vue
+++ hocus-focus/src/components/NotificationPermissions.vue
@@ -1,45 +1,77 @@
 <template>
-    <BaseModal 
-        v-if="!granted"
-        :popupClasses="[
-            'bg-pastel-yellow-100',
-            'border-pastel-yellow-500',
-        ]"
-    >
-        <template v-slot:body>
-            <div class="text-center">
-                We need permission to notify you when each interval ends!
-            </div>
-        </template>
+    <div>
+        <BaseModal 
+            v-if="!granted"
+            :popupClasses="[
+                'bg-pastel-yellow-100',
+                'border-pastel-yellow-500',
+            ]"
+        >
+            <template v-slot:body>
+                <div class="text-center">
+                    We need permission to notify you when each interval ends!
+                </div>
+            </template>
 
-        <template v-slot:footer>
-            <div class="flex justify-center gap-2">
-                <svg 
-                    xmlns="http://www.w3.org/2000/svg" 
-                    class="h-6 w-6" 
-                    fill="none" 
-                    viewBox="0 0 24 24" 
-                    stroke="currentColor" 
-                    stroke-width="2"
-                    @click="granted = true"
-                >
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
+            <template v-slot:footer>
+                <div class="flex justify-center gap-2">
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        class="h-6 w-6" 
+                        fill="none" 
+                        viewBox="0 0 24 24" 
+                        stroke="currentColor" 
+                        stroke-width="2"
+                        @click="granted = true"
+                    >
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
 
-                <svg 
-                    xmlns="http://www.w3.org/2000/svg" 
-                    class="h-6 w-6" 
-                    fill="none" 
-                    viewBox="0 0 24 24" 
-                    stroke="currentColor" 
-                    stroke-width="2"
-                    @click="requestPermission"
-                >
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-            </div>
-        </template>
-    </BaseModal>
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        class="h-6 w-6" 
+                        fill="none" 
+                        viewBox="0 0 24 24" 
+                        stroke="currentColor" 
+                        stroke-width="2"
+                        @click="requestPermission"
+                    >
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </div>
+            </template>
+        </BaseModal>
+
+        <BaseModal 
+            v-if="!hasNotifications"
+            :popupClasses="[
+                'bg-pastel-yellow-100',
+                'border-pastel-yellow-500',
+            ]"
+        >
+            <template v-slot:body>
+                <div class="text-center">
+                    Your browser does not support notifications so we will not be able to notify you when each interval ends.
+                </div>
+            </template>
+
+            <template v-slot:footer>
+                <div class="flex justify-center gap-2">
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        class="h-6 w-6" 
+                        fill="none" 
+                        viewBox="0 0 24 24" 
+                        stroke="currentColor" 
+                        stroke-width="2"
+                        @click="hasNotifications = true"
+                    >
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </div>
+            </template>
+        </BaseModal>
+    </div>
 </template>
 
 <script>
@@ -49,6 +81,7 @@ export default {
     name: "NotificationPermissions",
     data() {
         return {
+            hasNotifications: true,
             granted: true,
         }
     },
@@ -58,9 +91,8 @@ export default {
     mounted() {
         if ('Notification' in window) {
             this.granted = Notification.permission === 'granted';
-            console.log(this.granted);
         } else {
-            console.log('something about We cannot send you notifications when your time is up');
+            this.hasNotifications = false;
         }
     },
     methods: {
