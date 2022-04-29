9f0d12e3420e78067e1b1ed6178a283afe04c41f
Christina Nguyen <christina@terrier.tech>
2022-04-29 18:44:46 -0500

    style range sliders; remove outline from focused inputs

--- hocus-focus/src/components/BaseProgressSlider.vue
+++ hocus-focus/src/components/BaseProgressSlider.vue
@@ -58,4 +58,83 @@ export default {
 .complete {
     @apply bg-pastel-green-100;
 }
+
+/* clear default styles */
+input[type=range] {
+    -webkit-appearance: none;
+    @apply w-full;
+    @apply bg-transparent;
+}
+
+input[type=range]::-ms-track {
+    @apply w-full;
+    cursor: pointer;
+
+    @apply bg-transparent;
+    @apply border-transparent;
+    @apply text-transparent;
+}
+
+/* thumb slider stuffs */
+input[type=range]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    @apply bg-pastel-green-200;
+    @apply border-pastel-green-400;
+    @apply rounded-full;
+    @apply w-4;
+    @apply h-4;
+    @apply drop-shadow-sm;
+    cursor: pointer;
+    margin-top: -5px; /* chrome, idk */
+}
+
+input[type=range]::-moz-range-thumb {
+    @apply bg-pastel-green-200;
+    @apply border-pastel-green-400;
+    @apply rounded-full;
+    @apply w-4;
+    @apply h-4;
+    @apply drop-shadow-sm;
+    cursor: pointer;
+}
+
+input[type=range]::-ms-thumb {
+    @apply bg-pastel-green-200;
+    @apply border-pastel-green-400;
+    @apply rounded-full;
+    @apply w-4;
+    @apply h-4;
+    cursor: pointer;
+}
+
+input[type=range]:focus {
+    @apply outline-none;
+}
+
+/* track slider stuffs */
+input[type=range]::-webkit-slider-runnable-track {
+    @apply w-full;
+    @apply h-1.5;
+    @apply bg-pastel-yellow-300;
+    @apply drop-shadow-md;
+    @apply border-pastel-yellow-500;
+    @apply rounded-lg;
+    cursor: pointer;
+}
+
+input[type=range]:focus::-webkit-slider-runnable-track {
+    @apply bg-pastel-yellow-300;
+    @apply border-pastel-yellow-500;
+    @apply drop-shadow-md;
+}
+
+input[type=range]::-moz-range-track {
+    @apply w-full;
+    @apply h-1.5;
+    @apply bg-pastel-yellow-300;
+    @apply border-pastel-yellow-500;
+    @apply drop-shadow-md;
+    @apply rounded-lg;
+    cursor: pointer;
+}
 </style>
