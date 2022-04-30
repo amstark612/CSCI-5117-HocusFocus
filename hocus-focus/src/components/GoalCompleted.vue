df5b59b57359173a333744162159da31c8687289
kinzaahmed-web <kinza1013@yahoo.com>
2022-04-30 13:20:00 -0500

    separated summary logic into separate components

---
+++ hocus-focus/src/components/GoalCompleted.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class="card prompt">
+		<div class="flex gap-2 sm:gap-10 items-center">
+			<BaseIcon
+				:dArray="[
+					'M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z',
+				]"
+			/>
+			<h1 class="text-xl sm:text-3xl">Mission accomplished!</h1>
+			<BaseIcon
+				:dArray="[
+					'M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z',
+				]"
+			/>
+		</div>
+
+		<div class="sm:px-16">
+			Update your progress on any tasks and click below when you're ready to
+			view your session summary!
+		</div>
+
+		<div @click="openGoalSummary">
+			<BaseIcon
+				:properties="{
+					height: 'h-8',
+					width: 'w-8',
+					strokeWidth: '1.5',
+					classes: ['stroke-pastel-green-500'],
+				}"
+				:dArray="['M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z']"
+			/>
+			Ready!
+		</div>
+	</div>
+</template>
+
+<script>
+import BaseIcon from "@/components/BaseIcon.vue";
+export default {
+	name: "GoalCompleted",
+	props: {},
+	components: {
+		BaseIcon,
+	},
+	methods: {
+		openGoalSummary() {
+			this.$emit("showSummary", true);
+		},
+	},
+};
+</script>
+
+<style></style>
---
+++ hocus-focus/src/components/GoalSummaryModal.vue
@@ -0,0 +1,63 @@
+<template>
+	<BaseModal
+		:popupClasses="['bg-pastel-yellow-100', 'border-pastel-yellow-500']"
+	>
+		<template v-slot:body>
+			<div class="text-center">
+				<div class="sm:px-16">
+					You have concentrated for {{ focusTime / 60000 }} minutes
+				</div>
+
+				<br />
+				<div class="sm:px-16">You have been working on {{ taskIds }}</div>
+
+				<br />
+
+				<div class="flex justify-center" @click="close">
+					Start a new session &nbsp;
+					<BaseIcon
+						:properties="{
+							height: 'h-8',
+							width: 'w-8',
+							strokeWidth: '1.5',
+							classes: ['stroke-pastel-green-500'],
+						}"
+						:dArray="[
+							'M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z',
+						]"
+					/>
+				</div>
+			</div>
+		</template>
+
+		<template v-slot:footer> </template>
+	</BaseModal>
+</template>
+
+<script>
+import BaseIcon from "@/components/BaseIcon.vue";
+import BaseModal from "@/components/BaseModal.vue";
+export default {
+	name: "GoalSummaryModal",
+	data() {
+		return {
+			showSummary: false,
+		};
+	},
+	props: {
+		focusTime: Number,
+		taskIds: Array,
+	},
+	components: {
+		BaseIcon,
+		BaseModal,
+	},
+	methods: {
+		close() {
+			this.$emit("closeSummaryPrompt", false);
+		},
+	},
+};
+</script>
+
+<style></style>
