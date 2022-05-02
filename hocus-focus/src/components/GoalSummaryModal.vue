84ef4b7f3d620a5fc6408a83ab3ac820f8ad74e4
Christina Nguyen <christina@terrier.tech>
2022-05-01 19:13:49 -0500

    fix summary alignment

--- hocus-focus/src/components/GoalSummaryModal.vue
+++ hocus-focus/src/components/GoalSummaryModal.vue
@@ -3,23 +3,35 @@
 		:popupClasses="['bg-pastel-yellow-100', 'border-pastel-yellow-500']"
 	>
 		<template v-slot:body>
-			<div class="card1 timer">
+			<div class="card timer">
 				<div class="flex gap-2 sm:gap-10 items-center">
 					<BaseIcon
+                        :properties="{
+                            height: 'h-9',
+                            width: 'w-9',
+                            strokeWidth: '1.5',
+                        }"
 						:dArray="[
 							'M8 13v-1m4 1v-3m4 3V8M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z',
 						]"
 					/>
-					<h1 class="text-xl sm:text-3xl">focus goal summary</h1>
+					<h1 class="text-sm sm:text-3xl">focus session summary</h1>
 					<BaseIcon
+                        :properties="{
+                            height: 'h-9',
+                            width: 'w-9',
+                            strokeWidth: '1.5',
+                        }"
 						:dArray="[
 							'M8 13v-1m4 1v-3m4 3V8M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z',
 						]"
 					/>
 				</div>
+
 				<h3>{{ getDate() }}</h3>
+
 				<div class="wrapper">
-					<div class="card1 deck text-center">
+					<div class="card1 deck">
 						<div>
 							<h1 class="number">{{ Math.floor(focusTime / 60000) }}</h1>
 						</div>
@@ -28,7 +40,7 @@
 							<h3>minutes of focus</h3>
 						</div>
 					</div>
-					<div class="card1 deck text-5xl text-center">
+					<div class="card1 deck">
 						<div>
 							<h1 class="number">{{ numCompleted }}</h1>
 						</div>
@@ -37,7 +49,7 @@
 							<h3>tasks completed</h3>
 						</div>
 					</div>
-					<div class="card1 deck text-5xl text-center">
+					<div class="card1 deck">
 						<div>
 							<h1 class="number">{{ tasks.length }}</h1>
 						</div>
@@ -48,15 +60,15 @@
 					</div>
 				</div>
 
-				<div v-if="tasks.length">
+				<div v-if="tasks.length" class="w-full flex flex-col items-center justify-center">
 					<h2>task progress</h2>
-					<div class="card deck pb-2">
+					<div class="card deck">
 						<div
 							v-for="task in tasks"
 							:key="task.id"
-							class="grid grid-cols-3 gap-y-4"
+							class="flex justify-between mb-1"
 						>
-							<div id="text-left ml-2" class="col-span-2 flex items-center gap-2">
+							<div class="flex items-center gap-1">
 								<BaseIcon
 									:properties="{
 										height: 'h-6',
@@ -68,9 +80,9 @@
 										'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4',
 									]"
 								/>
-								{{ task.title }}
+								<div class="line-clamp">{{ task.title }}</div>
 							</div>
-							<div class="text-right mr-2">{{ task.progress }} %</div>
+							<div class="text-right mr-2">{{ task.progress }}%</div>
 						</div>
 					</div>
 				</div>
@@ -190,8 +202,12 @@ export default {
 	@apply my-2;
 	@apply rounded-lg;
 	@apply border;
-	@apply drop-shadow-lg;
+	@apply drop-shadow-md;
 	@apply text-sm;
+    @apply flex;
+    @apply flex-col;
+    @apply justify-between;
+    @apply text-center;
 }
 .timer {
 	@apply flex;
@@ -199,20 +215,19 @@ export default {
 	@apply gap-y-6;
 	@apply items-center;
 	@apply h-fit;
-	@apply mt-4;
 	@apply border-pastel-green-500;
 	@apply bg-pastel-green-100;
 	@apply text-pastel-green-500;
-	@apply sm:h-fit;
+    @apply p-6;
 }
 .wrapper {
 	@apply block;
 	@apply w-fit;
 	@apply h-fit;
 	@apply m-1;
-	@apply sm:grid;
-	@apply sm:grid-cols-3;
-	@apply sm:ml-0;
+    @apply sm:flex;
+    @apply sm:justify-center;
+    @apply sm:gap-4;
 }
 .deck {
 	@apply block;
